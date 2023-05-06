482fb1402a7219623fb5f3b44956f4a7d83d74ba
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2023-05-06 14:00:03 -0500

    Fixed bug in ComputeOffsets

--- ComputeOffsets.py
+++ ComputeOffsets.py
@@ -10,7 +10,8 @@ from .Profiles.Schneider19Profiles import DarkMatterOnly, DarkMatterBaryon
 
 class BaryonificationClass(object):
 
-    def __init__(self, DMO, DMB, ccl_cosmo, R_range = [1e-5, 50], N_samples = 500, epsilon_max = 4):
+    def __init__(self, DMO, DMB, ccl_cosmo, R_range = [1e-5, 50], N_samples = 500, epsilon_max = 4,
+                 mass_def = ccl.halos.massdef.MassDef(200, 'critical')):
 
 
         self.DMO = DMO
@@ -19,6 +20,7 @@ class BaryonificationClass(object):
         self.R_range     = R_range
         self.epsilon_max = epsilon_max
         self.N_samples   = N_samples
+        self.mass_def    = mass_def
 
         self.setup_interpolator()
 
@@ -45,8 +47,8 @@ class BaryonificationClass(object):
             for j in range(z_range.size):
 
                 #Extra factor of "a" accounts for projection in ccl being done in comoving, not physical units
-                M_DMO_interp[j, i, :] = self.get_masses(self.DMO, r, M_range[i], 1/(1 + z_range[j]), mass_def = mass_def)
-                M_DMB_interp[j, i, :] = self.get_masses(self.DMB, r, M_range[i], 1/(1 + z_range[j]), mass_def = mass_def)
+                M_DMO_interp[j, i, :] = self.get_masses(self.DMO, r, M_range[i], 1/(1 + z_range[j]), mass_def = self.mass_def)
+                M_DMB_interp[j, i, :] = self.get_masses(self.DMB, r, M_range[i], 1/(1 + z_range[j]), mass_def = self.mass_def)
 
                 log_r_new_interp[j, i, :] = np.interp(np.log(M_DMB_range_interp), np.log(M_DMB_interp[j, i]), np.log(r))
 
@@ -59,7 +61,7 @@ class BaryonificationClass(object):
         return 0
 
 
-    def displacements(self, x, M, a, mass_def = ccl.halos.massdef.MassDef(200, 'critical')):
+    def displacements(self, x, M, a):
 
         r    = np.geomspace(self.R_range[0], self.R_range[1], self.N_samples)
         dlnr = np.log(r[1]) - np.log(r[0])
@@ -67,7 +69,7 @@ class BaryonificationClass(object):
         z = 1/a - 1
 
         M_use = np.atleast_1d(M)
-        R     = mass_def.get_radius(cosmo, M_use, a)/a #in comoving Mpc
+        R     = self.mass_def.get_radius(cosmo, M_use, a)/a #in comoving Mpc
 
         offset = np.zeros_like(x)
         inside = (x > self.R_range[0]) & (x < self.epsilon_max*R)
