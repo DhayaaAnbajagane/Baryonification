4a9915141dd20af3a10233cff87a6455bba5d2bc
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2023-05-06 13:28:29 -0500

    WIP updates

--- Profiles/__init__.py
+++ Profiles/__init__.py
@@ -1,2 +1,2 @@
-from Baryonification.Profiles.Schneider19Profiles import SchneiderProfiles, DarkMatter, TwoHalo, Stars, Gas, CollisionlessMatter, DarkMatterOnly, DarkMatterBaryon
-from Baryonification.Profiles.tSZProfiles import Pressure
+from ..Profiles.Schneider19Profiles import SchneiderProfiles, DarkMatter, TwoHalo, Stars, Gas, CollisionlessMatter, DarkMatterOnly, DarkMatterBaryon
+from ..Profiles.tSZProfiles import Pressure
