f295525a3f6fde93c46e1c26e5f38bfed98711d1
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2023-05-07 07:40:04 -0500

    Restructuring

--- Profiles/__init__.py
+++ Profiles/__init__.py
@@ -1,2 +1,3 @@
-from ..Profiles.Schneider19Profiles import SchneiderProfiles, DarkMatter, TwoHalo, Stars, Gas, CollisionlessMatter, DarkMatterOnly, DarkMatterBaryon
-from ..Profiles.ThermodynamicProfiles import BattagliaPressure, Pressure
+from ..Profiles.Schneider19 import SchneiderProfiles, DarkMatter, TwoHalo, Stars, Gas, CollisionlessMatter, DarkMatterOnly, DarkMatterBaryon
+from ..Profiles.Thermodynamic import BattagliaPressure, Pressure
+from ..Profiles.BaryonCorrection import Baryonification3D, Baryonification2D
