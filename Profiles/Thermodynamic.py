91bc04b896a6ea9742d608b60fe1d451b2b199d7
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2023-05-09 14:12:46 -0500

    New thermo codes. Safety commit too

--- Profiles/Thermodynamic.py
+++ Profiles/Thermodynamic.py
@@ -6,7 +6,7 @@ from scipy import interpolate
 from astropy.cosmology import z_at_value, FlatLambdaCDM, FlatwCDM
 from astropy import units as u
 
-from ..Profiles.Schneider19 import SchneiderProfiles, Gas, DarkMatterBaryon
+from ..Profiles.Schneider19 import SchneiderProfiles, Gas, DarkMatterBaryon, TwoHalo
 
 
 #Define relevant physical constants
@@ -14,10 +14,12 @@ Msun_to_Kg = ccl.physical_constants.SOLAR_MASS
 Mpc_to_m   = ccl.physical_constants.MPC_TO_METER
 G          = ccl.physical_constants.GNEWT / Mpc_to_m**3 * Msun_to_Kg
 m_to_cm    = 1e2
+kb_in_ev   = ccl.physical_constants.KBOLTZ / ccl.physical_constants.EV_IN_J
 
 #Just define some useful conversions/constants
 sigma_T = 6.652458e-29 / Mpc_to_m**2
 m_e     = 9.10938e-31 / Msun_to_Kg
+m_p     = 1.67262e-27 / Msun_to_Kg
 c       = 2.99792458e8 / Mpc_to_m
 
 #Thermodynamic/abundance quantities
@@ -196,7 +198,6 @@ class Pressure(SchneiderProfiles):
         
         self.nonthermal_model = nonthermal_model
         super().__init__(**kwargs)
-        
     
     def _real(self, cosmo, r, M, a, mass_def = ccl.halos.massdef.MassDef(200, 'critical')):
         
@@ -215,9 +216,15 @@ class Pressure(SchneiderProfiles):
                                       A = self.A, M1 = self.M1, eta_star = self.eta_star, eta_cga = self.eta_cga, epsilon_h = self.epsilon_h,
                                       p = self.p, q = self.q, xi_mm = self.xi_mm)
         Gas_prof   = Gas(theta_ej = self.theta_ej, theta_co = self.theta_co, M_c = self.M_c, mu = self.mu, A = self.A, M1 = self.M1, eta_star = self.eta_star, epsilon = self.epsilon)
+        
 
         rho_total  = Total_prof.real(cosmo, r_integral, M, a, mass_def = mass_def)
         rho_gas    = Gas_prof.real(cosmo, r_integral, M, a, mass_def = mass_def)
+        
+        #Remove two halo term from total matter profile. 
+        #Only the halo (one halo term) is in equilibrium in our model
+        TwoHalo_prof = TwoHalo(p = self.p, q = self.q, xi_mm = self.xi_mm)
+        rho_total   -= TwoHalo_prof.real(cosmo, r_integral, M, a, mass_def = mass_def)
         
         dlnr    = np.log(r_integral[1]) - np.log(r_integral[0])
         M_total = 4 * np.pi * np.cumsum(r_integral**3 * rho_total * dlnr, axis = -1)
@@ -277,6 +284,113 @@ class Pressure(SchneiderProfiles):
             raise ValueError("Need to use model = None or model = Green20 No other model implemented so far.")
 
         return nth
+
+class GasNumberDensity(SchneiderProfiles):
+    
+    def __init__(self, mean_molecular_weight = 1.15, **kwargs):
+        
+        self.mean_molecular_weight = mean_molecular_weight
+        super().__init__(**kwargs)
+        
+    
+    def _real(self, cosmo, r, M, a, mass_def = ccl.halos.massdef.MassDef(200, 'critical')):
+        
+        
+        r_use = np.atleast_1d(r)
+        M_use = np.atleast_1d(M)
+
+        z = 1/a - 1
+
+        R = mass_def.get_radius(cosmo, M_use, a)/a #in comoving Mpc
+
+        rho  = Gas(theta_ej = self.theta_ej, theta_co = self.theta_co, M_c = self.M_c, mu = self.mu, A = self.A, M1 = self.M1, eta_star = self.eta_star, epsilon = self.epsilon)
+        rho  = rho.real(cosmo, r_use, M, a, mass_def = mass_def)
+        prof = rho / (self.mean_molecular_weight * m_p) / (Mpc_to_m * m_to_cm)**3
+        
+        #Handle dimensions so input dimensions are mirrored in the output
+        if np.ndim(r) == 0:
+            prof = np.squeeze(prof, axis=-1)
+        if np.ndim(M) == 0:
+            prof = np.squeeze(prof, axis=0)
+
+        assert np.all(prof >= 0), "Something went wrong. Temperature is negative in some places"
+
+        return prof
+
+
+class Temperature(Pressure):
+    
+    def _real(self, cosmo, r, M, a, mass_def = ccl.halos.massdef.MassDef(200, 'critical')):
+        
+        
+        r_use = np.atleast_1d(r)
+        M_use = np.atleast_1d(M)
+
+        z = 1/a - 1
+
+        R = mass_def.get_radius(cosmo, M_use, a)/a #in comoving Mpc
+
+        P   = Pressure(nonthermal_model = self.nonthermal_model, epsilon = self.epsilon, a = self.a, n = self.n,
+                                      theta_ej = self.theta_ej, theta_co = self.theta_co, M_c = self.M_c, mu = self.mu,
+                                      A = self.A, M1 = self.M1, eta_star = self.eta_star, eta_cga = self.eta_cga, epsilon_h = self.epsilon_h,
+                                      p = self.p, q = self.q, xi_mm = self.xi_mm)
+        n   = GasNumberDensity(theta_ej = self.theta_ej, theta_co = self.theta_co, M_c = self.M_c, mu = self.mu, A = self.A, M1 = self.M1, eta_star = self.eta_star, epsilon = self.epsilon)
+
+        P   = P.real(cosmo, r_use, M, a, mass_def = mass_def)
+        n   = n.real(cosmo, r_use, M, a, mass_def = mass_def)
+        
+        prof = P/(n * kb_in_ev)
+        
+        #Handle dimensions so input dimensions are mirrored in the output
+        if np.ndim(r) == 0:
+            prof = np.squeeze(prof, axis=-1)
+        if np.ndim(M) == 0:
+            prof = np.squeeze(prof, axis=0)
+
+        assert np.all(prof >= 0), "Something went wrong. Temperature is negative in some places"
+
+        return prof
+
+
+class CustomTemperature(ccl.halos.profiles.HaloProfile):
+    
+    def __init__(self, Pressure, GasNumberDensity):
+        
+        self.Pressure = Pressure
+        self.GasNumberDensity = GasNumberDensity
+        super().__init__()
+        
+    
+    def _real(self, cosmo, r, M, a, mass_def = ccl.halos.massdef.MassDef(200, 'critical')):
+        
+        
+        r_use = np.atleast_1d(r)
+        M_use = np.atleast_1d(M)
+
+        z = 1/a - 1
+
+        R = mass_def.get_radius(cosmo, M_use, a)/a #in comoving Mpc
+
+        P   = self.Pressure(nonthermal_model = self.nonthermal_model, epsilon = self.epsilon, a = self.a, n = self.n,
+                                      theta_ej = self.theta_ej, theta_co = self.theta_co, M_c = self.M_c, mu = self.mu,
+                                      A = self.A, M1 = self.M1, eta_star = self.eta_star, eta_cga = self.eta_cga, epsilon_h = self.epsilon_h,
+                                      p = self.p, q = self.q, xi_mm = self.xi_mm)
+        n   = self.GasNumberDensity(theta_ej = self.theta_ej, theta_co = self.theta_co, M_c = self.M_c, mu = self.mu, A = self.A, M1 = self.M1, eta_star = self.eta_star, epsilon = self.epsilon)
+
+        P   = P.real(cosmo, r_use, M, a, mass_def = mass_def)
+        n   = n.real(cosmo, r_use, M, a, mass_def = mass_def)
+        
+        prof = P/(n * kb_in_ev)
+        
+        #Handle dimensions so input dimensions are mirrored in the output
+        if np.ndim(r) == 0:
+            prof = np.squeeze(prof, axis=-1)
+        if np.ndim(M) == 0:
+            prof = np.squeeze(prof, axis=0)
+
+        assert np.all(prof >= 0), "Something went wrong. Temperature is negative in some places"
+
+        return prof
     
     
 #Base class to generate SZ profiles
@@ -320,3 +434,40 @@ class ThermalSZ(object):
     def real(self, cosmo, r, M, a, mass_def = ccl.halos.massdef.MassDef(200, 'critical')):
     
         return np.zeros_like(r)
+    
+    
+    
+class XrayLuminosity(ccl.halos.profiles.HaloProfile):
+    
+    
+    def __init__(self, Temperature, GasNumberDensity):
+        
+        self.Temperature      = Temperature
+        self.GasNumberDensity = GasNumberDensity
+        self.epsilon_max      = epsilon_max
+        
+        super().__init__()
+        
+    
+    def _real(self, cosmo, r, M, a, mass_def = ccl.halos.massdef.MassDef(200, 'critical')):
+        
+        
+        r_use = np.atleast_1d(r)
+        M_use = np.atleast_1d(M)
+
+        z = 1/a - 1
+
+        R = mass_def.get_radius(cosmo, M_use, a)/a #in comoving Mpc
+
+        T   = self.Temperature.real(cosmo, r_use, M, a, mass_def = mass_def)
+        n   = self.GasNumberDensity.real(cosmo, r_use, M, a, mass_def = mass_def)
+        
+        prof = n**2*T
+        
+        #Handle dimensions so input dimensions are mirrored in the output
+        if np.ndim(r) == 0:
+            prof = np.squeeze(prof, axis=-1)
+        if np.ndim(M) == 0:
+            prof = np.squeeze(prof, axis=0)
+        
+        return prof
