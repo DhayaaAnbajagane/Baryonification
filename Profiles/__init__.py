b06ac74052f0ac643b7720bbeb043d9644d73572
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2023-06-10 17:56:11 -0500

    BUG: Fix imports

--- Profiles/__init__.py
+++ Profiles/__init__.py
@@ -1,4 +1,4 @@
 from ..Profiles.Schneider19 import SchneiderProfiles, DarkMatter, TwoHalo, Stars, Gas, CollisionlessMatter, DarkMatterOnly, DarkMatterBaryon
 from ..Profiles.Thermodynamic import Pressure, ThermalSZ
 from ..Profiles.BaryonCorrection import Baryonification3D, Baryonification2D
-from ..Profiles.Battaglia import BattagliaPressure, BattagliaGasNumberDensity
+from ..Profiles.Battaglia import BattagliaPressure, BattagliaGas
