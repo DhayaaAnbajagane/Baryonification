52e58b7f339d830d560376b6e3cedec2c4553d5e
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2023-06-11 14:53:09 -0500

    Quick fix imports

--- Runners/__init__.py
+++ Runners/__init__.py
@@ -1,2 +1,2 @@
-from ..Runners.HealpixRunner import DefaultRunner, BaryonifyShell, PaintThermalSZShell
-from ..Runners.Map2DRunner import DefaultRunnerGrid, BaryonifyGrid, PaintThermalSZGrid
+from ..Runners.HealpixRunner import DefaultRunner, BaryonifyShell, PaintThermalSZShell, PaintProfilesShell
+from ..Runners.Map2DRunner import DefaultRunnerGrid, BaryonifyGrid, PaintThermalSZGrid, PaintProfilesGrid
