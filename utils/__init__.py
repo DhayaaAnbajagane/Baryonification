5ff1fc92e028a16f6f8866e5d1b0adb582c77696
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2024-07-05 23:13:41 -0500

    Fix init bug

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
 from ..utils.io import HaloLightConeCatalog, LightconeShell, HaloNDCatalog, GriddedMap, ParticleSnapshot
-from ..utils.Tabulate import TabulatedProfile
+from ..utils.Tabulate import TabulatedProfile, ParamTabulatedProfile
 from ..utils.Parallelize import SimpleParallel, SplitJoinParallel
 from ..utils.debug import log_time
 from ..utils.Pixel import ConvolvedProfile, GridPixelApprox, HealPixel
