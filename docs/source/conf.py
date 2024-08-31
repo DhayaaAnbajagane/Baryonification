4a5c12eb7839106af2ca2a4c4be254ac006c28af
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2024-08-31 09:17:03 -0500

    move paths

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ def run_apidoc(_):
     import subprocess
 
     # Define the replacement strings
-    subprocess.check_call(['mv', '../latest', '../Baryonification'])
+    subprocess.check_call(['mv', '../../../latest', '../../../Baryonification'])
     
     cmd_path = 'sphinx-apidoc'
     if hasattr(sys, 'real_prefix'):  # Check to see if we are in a virtualenv
